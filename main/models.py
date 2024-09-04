8ba1dfe843564b231b0587e0fbc226a1fa39e1d9
Ilham Ghani <ghanijr7gmail.com>
2024-09-04 16:01:21 +0700

    change: models

--- main/models.py
+++ main/models.py
@@ -7,12 +7,6 @@ class MeowEntry(models.Model):
     name = models.CharField(max_length=255)
     price = models.IntegerField
     description = models.TextField
-
-
-    # time = models.DateField(auto_now_add=True)
-    # feelings = models.TextField()
-    # mood_intensity = models.IntegerField()
-
-    # @property
-    # def is_mood_strong(self):
-    #     return self.mood_intensity > 5
+    species = models.CharField(max_length=255)
+    colour = models.CharField(max_length=255)
+    age = models.IntegerField
